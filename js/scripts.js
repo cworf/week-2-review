718fbf5ec00f7f5b54cd8778473db3f4b792fb39
Colin Worf <colin.worf@gmail.com>
2017-10-20 15:02:12 -0700

    create conditional logic in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(function(){
   $('#main').submit(function(event){
     event.preventDefault();
+    val f-name = $('#f-name').val
     var question1 = $('input:radio[name=q1]:checked').val();
     var question2 = $('input:radio[name=q2]:checked').val();
     var question3 = $('input:radio[name=q3]:checked').val();
@@ -14,26 +15,40 @@ $(function(){
     var match2 = concat.match(/2/g).length;
     var match1 = concat.match(/1/g).length;
 
+    $('#student').text(name);
+
     if ( match1 >= 3 ) {
-
+      $('#result-title, #class-name').text("Design/CSS");
+      $('#info').attr("href", "https://www.epicodus.com/design");
     } else if ( match2 >= 3 ){
-
+      $('#result-title, #class-name').text("Ruby");
+      $('#info').attr("href", "https://www.epicodus.com/ruby");
     } else if ( match3 >= 3 ){
-
+      $('#result-title, #class-name').text("PHP");
+      $('#info').attr("href", "https://www.epicodus.com/php");
     } else if ( match4 >= 3 ){
-
+      $('#result-title, #class-name').text("Java");
+      $('#info').attr("href", "https://www.epicodus.com/java");
     } else if ( match5 >= 3 ){
-
+      $('#result-title, #class-name').text("C#");
+      $('#info').attr("href", "https://www.epicodus.com/c-sharp");
     } else if ( match1 = 2 ){
-
+      $('#result-title, #class-name').text("Design/CSS");
+      $('#info').attr("href", "https://www.epicodus.com/design");
     } else if ( match2 = 2 ){
-
+      $('#result-title, #class-name').text("Ruby");
+      $('#info').attr("href", "https://www.epicodus.com/ruby");
     } else if ( match3 = 2 ){
-
+      $('#result-title, #class-name').text("PHP");
+      $('#info').attr("href", "https://www.epicodus.com/php");
     } else if ( match4 = 2 ){
-
+      $('#result-title, #class-name').text("Java");
+      $('#info').attr("href", "https://www.epicodus.com/java");
     } else if ( match5 = 2 ){
-
+      $('#result-title, #class-name').text("C#");
+      $('#info').attr("href", "https://www.epicodus.com/c-sharp");
+    } else {
+      alert("Results inconclusive, please select better choices to enrole, you have 3 more chances")
     };
 
 
